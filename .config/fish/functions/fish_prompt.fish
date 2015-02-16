6829b23456b04778275c3ecc1e56ad5cc6eda896
Henrik Nygren <nygrenh@gmail.com>
2015-02-16 20:49:18 +0200

    Fish prompt: don't escape speces

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,10 +1,9 @@
 function fish_prompt
   set -l home_escaped (echo -n $HOME | sed 's/\//\\\\\//g')
-  set -l min_pwd (echo -n $PWD | sed "s/^$home_escaped/~/" | sed 's/ /%20/g')
+  set -l min_pwd (echo -n $PWD | sed "s/^$home_escaped/~/")
   set -l cyan (set_color -o cyan)
   set -l green (set_color -o green)
   set -l blue (set_color -o blue)
-  
-  # echo $pwd '$ '
+
   printf '%s%s %s%s ' "$blue" "$min_pwd" "$green" "\$"
 end
