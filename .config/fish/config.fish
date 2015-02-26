16c827164a9c553f1295643048d48a7bb50aac7c
Henrik Nygren <nygrenh@gmail.com>
2015-02-26 23:39:59 +0200

    Fish config: Add a couple of test abbreviations

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,3 +2,12 @@ set fish_greeting ''
 set GOPATH $HOME/Code/go
 set PATH $GOPATH/bin $PATH
 set PATH (ruby -rubygems -e "puts Gem.user_dir")/bin $PATH
+
+# Abbreviations
+set -e _a
+
+set _a $_a 'be=bundle exec'
+set _a $_a 'gc=git commit'
+
+set fish_user_abbreviations $_a
+set -e _a
